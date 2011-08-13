a331cc5749ce753d27c43d00acb9f02fdd58de70
Konrad Korzec <korzec@gmail.com>
2011-08-13 19:58:05 +0100

    Added initialization to CubeIO

--- CubeIO.cpp
+++ CubeIO.cpp
@@ -7,3 +7,55 @@
 
 #include "CubeIO.h"
 
+CubeIO::CubeIO() : inputFile(NULL), outputFile(NULL) {}
+
+CubeIO::CubeIO(std::string fileName, bool isWrite) : inputFile(NULL), outputFile(NULL)
+{
+    Init(fileName, isWrite);
+}
+bool CubeIO::Init(std::string fileName, bool isWrite)
+{
+    if(outputFile)
+        outputFile->close();
+    if(inputFile)
+        inputFile->close();
+    inputFile = NULL;
+    outputFile = NULL;
+    if(isWrite)
+    {
+        outputFile = new std::ofstream(fileName.c_str(), std::ios::out | std::ios::binary);
+        return ! outputFile->fail();
+    }
+    else
+    {
+        inputFile = new std::ifstream(fileName.c_str(), std::ios::in | std::ios::binary);
+        return ! inputFile->fail();
+    }
+    
+}
+
+bool CubeIO::WriteSequenceHeader(CodingParams)
+{
+    //write coding Params to a stream
+}
+bool CubeIO::WriteCubeHeader(int cubeNumber)
+{
+    
+}
+bool CubeIO::WritePacket(Packet)
+{
+    
+}
+
+CodingParams CubeIO::ReadSequenceHeader()
+{
+    
+}
+int CubeIO::ReadCubeHeader()
+{
+    
+}
+Packet CubeIO::ReadPacket()
+{
+    
+}
--- CubeIO.h
+++ CubeIO.h
@@ -8,13 +8,30 @@
 #ifndef CUBEIO_H
 #define	CUBEIO_H
 
+#include "types.h"
+#include "Packet.h"
+#include <string>
+#include <iostream>
+#include <fstream>
+
 /// Reads and writes data encoded with cube codec
 class CubeIO
 {
 public:
-    bool WritePacket();
+    CubeIO();
+    CubeIO(std::string fileName, bool isWrite);
+    bool Init(std::string fileName, bool isWrite);
+    
+    bool WriteSequenceHeader(CodingParams);
+    bool WriteCubeHeader(int cubeNumber);
+    bool WritePacket(Packet);
+    
+    CodingParams ReadSequenceHeader();
+    int ReadCubeHeader();
+    Packet ReadPacket();
 private:
-
+    std::ifstream* inputFile;
+    std::ofstream* outputFile;      
 };
 
 #endif	/* CUBEIO_H */
